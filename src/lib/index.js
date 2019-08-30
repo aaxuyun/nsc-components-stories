96543af179160d9ff345c50fc90e75e5844f348a
aaxuyun <784266498@qq.com>
2019-08-30 18:31:24 +0800

    update

--- src/lib/index.js
+++ src/lib/index.js
@@ -15,6 +15,19 @@ import * as ArcmapConstants from './components/arcmap/constants'
 import * as ArcmapUtils from './components/arcmap/utils'
 import * as ArcmapUtils from './components/arcmap/utils'
 
+import ColorPicker from './components/ColorPickerPopover'
+import CtxMenu from './components/ContextMenu'
+import DragM from './components/DragMove'
+import Drawer from './components/Drawer'
+import Dropdown from './components/Dropdown'
+import DynamicMountComponent from './components/DynamicMountComponent'
+import IFrame from './components/IFrame'
+import contentEditable from './components/InlineEditComponent'
+import Menu from './components/Menu'
+import PageLoading from './components/PageLoading'
+import PropertiesTable from './components/PropertiesTable'
+import TextEllipsis from './components/TextEllipsis'
+import Toolbar from './components/Toolbar'
 
 const NSC = { TooltipButton,Dropdown,Toolbar }
 export default NSC
